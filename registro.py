ed17af093ec1cbb61a6b785cc675a96eba3c0369
Miguel Ramos <return.infox@gmail.com>
2024-03-07 20:30:23 -0300

    terminada la funcion de registrar eventos

--- registro.py
+++ registro.py
@@ -1,2 +1,11 @@
 from datetime import datetime
+# Estructura de datos para almacenar los eventos
 eventos = []
+
+def registrar_eventos(nombre, fecha, descripcion):
+    for evento in eventos:
+        if evento['nombre'] == nombre and evento['fecha'] == fecha:
+            print("Ya existe un evento con este nombre y con la misma fecha!")
+            return
+    eventos.append({"nombre": nombre, "fecha": fecha, "descripcion": descripcion})
+    print("Evento registrado con éxito")
